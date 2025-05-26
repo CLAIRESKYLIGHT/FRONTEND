40a2ac5f7e3cac4f749e50334c0fbaae385a8d7a
JBalute <josiebalute895@gmail.com>
2025-05-26 14:25:26 +0800

    fixing the layout

--- src/app/components/ClientThemeProvider.js
+++ src/app/components/ClientThemeProvider.js
@@ -1,12 +1,11 @@
 'use client'
 
 import { ThemeProvider } from '../context/ThemeContext'
-import Sidebar from "./Sidebar"
 
 export default function ClientThemeProvider({ children }) {
   return (
     <ThemeProvider>
-      <Sidebar>{children}</Sidebar>
+      {children}
     </ThemeProvider>
   )
-} 
+}
